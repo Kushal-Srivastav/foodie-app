f0fc5a7061ae0e5fbc7f57d5b239794f8be93095
Kushal-Srivastav <kushalsrijan17@gmail.com>
2025-04-19 11:26:09 +0530

    added slideshow, design looks better, some modification in ts.config

---
+++ src/app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+// localhost:3000/meals/my-meal or something would activate the page.js file
+//inside the slug, bu tif we pass any folder path then next js is smart enough to 
+// call it separately and m=not through the activation of the slug.
+
+export default function MealDetailsPage(){
+    return <h1>Meal Details</h1>
+}
