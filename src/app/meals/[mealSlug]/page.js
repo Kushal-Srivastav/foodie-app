c4ead3406aa11d43bf37a9237a15c907edde022d
Kushal-Srivastav <kushalsrijan17@gmail.com>
2025-04-21 09:40:20 +0530

    database, image storing, form set up

--- src/app/meals/[mealSlug]/page.js
+++ src/app/meals/[mealSlug]/page.js
@@ -1,7 +1,35 @@
 // localhost:3000/meals/my-meal or something would activate the page.js file
 //inside the slug, bu tif we pass any folder path then next js is smart enough to 
 // call it separately and m=not through the activation of the slug.
+import { getMeal } from '@/lib/meals'
+import classes from './page.module.css'
+import Image from 'next/image'
+import { notFound } from 'next/navigation'
+export default function MealDetailsPage({params}){ //params here will have the value of the address
+    //passed in the bar whereas the slug will have the key to that value
+   const meal =  getMeal(params.mealSlug)
+   if(!meal){
+    notFound();
+   }
+   meal.instructions = meal.instructions.replace(/\n/g, '<br/>')
+    return <>
+    <header className={classes.header}>
+        <div className={classes.image}>
+            <Image src={meal.image} alt={meal.title} fill/>
+        </div>
+        <div className={classes.headerText}>
+            <h1>{meal.title}</h1>
+           <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+           </p>
 
-export default function MealDetailsPage(){
-    return <h1>Meal Details</h1>
+           <p className={classes.summary}>{meal.summary}</p>
+        </div>
+    </header>
+    <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+        }}></p>
+    </main>
+    </>
 }
